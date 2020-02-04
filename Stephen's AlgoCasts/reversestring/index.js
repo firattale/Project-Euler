383678f0f2f4c941caeefbb546ebc0fc86890032
Fırat Tale <firat.tale@betbull.com>
2020-02-04 22:06:15 +0300

    Add other solutions to reversestring

--- Stephen's AlgoCasts/reversestring/index.js
+++ Stephen's AlgoCasts/reversestring/index.js
@@ -6,8 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// Solution 1
 function reverse(str) {
     return str.split("").reverse().join("");
 }
+
+// Solution 2
+
+// function reverse(str) {
+//     return str.split("").reduce((reversed, character) => character + reversed, "");
+// }
+
+// Solution 3
+
+// function reverse(str) {
+//     let reversed = "";
+//     for (const character of str) {
+//         reversed = character + reversed
+//     }
+//     return reversed;
+// }
+
+
 
 module.exports = reverse;
