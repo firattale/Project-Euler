86fa62f3c555244440cf3a99319cb56c77dc31a5
Fırat Tale <firat.tale@betbull.com>
2020-02-20 23:32:58 +0300

    Add Fib Solution

---
+++ Stephen's AlgoCasts/fib/index.js
@@ -0,0 +1,45 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+// iterative solution
+
+function fib(n) {
+    let fibArr = [0,1]
+    for (let i = 2; i <= n; i++) {
+      const num = fibArr[i-1] + fibArr[i-2];
+      fibArr.push(num);
+    }
+    return fibArr[n];
+}
+
+// memoized recursive solution
+
+function slowFib(n) {
+    if (n < 2) {
+        return n;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+function memo(fn) {
+    const cache = {};
+    return function (...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    }
+}
+const fib = memo(slowFib);
+
+module.exports = fib;
